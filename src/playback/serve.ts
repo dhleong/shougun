576a8092252b02135e36cb0a362906c282abb7f3
Daniel Leong <me@dhleong.net>
2019-05-02 08:46:26 -0400

    Prevent shutting down the server while handling a request

--- src/playback/serve.ts
+++ src/playback/serve.ts
@@ -37,6 +37,7 @@ export class Server implements IServer {
 
     private media: {[id: string]: ILocalMedia} = {};
     private activeStreams: {[id: string]: number} = {};
+    private activeRequests = 0;
 
     public close() {
         const s = this.server;
@@ -76,58 +77,15 @@ export class Server implements IServer {
             maxParamLength: 512,
         });
         server.get("/playable/id/:id", async (req, reply) => {
-            const id = req.params.id;
-            debug("request playable @", id);
-
-            const media = this.media[id];
-            if (!media) throw new Error("No such media");
-            let toPlay = media;
-
-            const { queueIndex } = req.query;
-            if (queueIndex !== undefined) {
-                debug("pull actual playable from queue @", queueIndex);
-
-                // following queue?
-                // HAX: there's probably a better way to do this...
-                const playable = media as ServedPlayable;
-                const queue = await playable.loadQueueAround(context);
-                toPlay = await context.discovery.createPlayable(
-                    context,
-                    queue[queueIndex],
-                ) as ServedPlayable;
-
-                debug("  -> ", toPlay);
-
-                // MORE HAX: this startTime is from the original media, probably
-                delete req.query.startTime;
+            try {
+                debug(">> active request!");
+                ++this.activeRequests;
+                return await this.handlePlayableRequest(context, req, reply);
+            } finally {
+                debug("<< end request");
+                --this.activeRequests;
+                this.checkStreamsForShutdown();
             }
-
-            const onStreamEnded = () => {
-                this.onStreamEnded(toPlay);
-            };
-
-            const { contentType, localPath } = toPlay;
-            let stream: NodeJS.ReadableStream;
-            if (contentType === "video/mp4") {
-                stream = await serveMp4(
-                    req, reply, localPath,
-                );
-            } else {
-                const startTime = req.query.startTime || 0;
-
-                stream = await serveTranscoded(
-                    req, reply, localPath, startTime,
-                );
-            }
-
-            debug("got stream", stream);
-            stream.once("close", onStreamEnded);
-
-            // if we get here, the stream was created without error
-            this.onStreamStarted(toPlay);
-
-            // serve!
-            return stream;
         });
 
         this.server = server;
@@ -143,6 +101,72 @@ export class Server implements IServer {
         debug("serving on", actual);
         this.address = actual;
         return actual;
+    }
+
+    private async handlePlayableRequest(
+        context: Context,
+        req: fastify.FastifyRequest<any>,
+        reply: fastify.FastifyReply<any>,
+    ) {
+        const id = req.params.id;
+        debug("request playable @", id);
+
+        const media = this.media[id];
+        if (!media) throw new Error("No such media");
+        let toPlay = media;
+
+        const { queueIndex } = req.query;
+        if (queueIndex !== undefined) {
+            debug("pull actual playable from queue @", queueIndex);
+
+            // following queue?
+            // HAX: there's probably a better way to do this...
+            const playable = media as ServedPlayable;
+            const queue = await playable.loadQueueAround(context);
+            toPlay = await context.discovery.createPlayable(
+                context,
+                queue[queueIndex],
+            ) as ServedPlayable;
+
+            // MORE HAX: this startTime is from the original media, probably
+            delete req.query.startTime;
+
+            debug("  -> ", toPlay.id, " @ ", toPlay.localPath);
+
+            // NOTE: we still use `media` for onStreamStarted and
+            // onStreamEnded, since those protect its existence in
+            // the `this.media` map; we need to keep `media` there
+            // since all the queued items are based on it. If `media`
+            // were removed in favor of `toPlay`, we wouldn't be able
+            // to play any of the other items in the queue anymore!
+        }
+
+        const onStreamEnded = () => {
+            this.onStreamEnded(media);
+        };
+
+        const { contentType, localPath } = toPlay;
+        let stream: NodeJS.ReadableStream;
+        if (contentType === "video/mp4") {
+            stream = await serveMp4(
+                req, reply, localPath,
+            );
+        } else {
+            const startTime = req.query.startTime || 0;
+
+            stream = await serveTranscoded(
+                req, reply, localPath, startTime,
+            );
+        }
+
+        debug("got stream", stream);
+        stream.once("close", onStreamEnded);
+
+        // if we get here, the stream was created without error
+        this.onStreamStarted(media);
+
+        // serve!
+        return stream;
     }
 
     private onStreamStarted(media: ILocalMedia) {
@@ -167,6 +191,11 @@ export class Server implements IServer {
     }
 
     private checkStreamsForShutdown() {
+        if (this.activeRequests) {
+            debug(`found active requests; stay alive`);
+            return;
+        }
+
         for (const id of Object.keys(this.activeStreams)) {
             if (this.activeStreams[id] > 0) {
                 // still active streams
