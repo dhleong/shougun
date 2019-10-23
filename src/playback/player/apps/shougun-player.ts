cfb713d1dcad9cbeed7cef450fd2ff5dc9677356
Daniel Leong <me@dhleong.net>
2019-10-23 10:07:04 -0400

    Use prod player app ID by default

--- src/playback/player/apps/shougun-player.ts
+++ src/playback/player/apps/shougun-player.ts
@@ -1,11 +1,18 @@
+import _debug from "debug";
+
 import { IDevice } from "babbling";
 
 import { GenericMediaReceiverApp } from "./generic";
 
+const DEV_ID = "81066132";
+const PROD_ID = "D85F931E";
+
 export class ShougunPlayerApp extends GenericMediaReceiverApp {
     constructor(device: IDevice) {
         super(device, {
-            appId: "81066132", // dev ID
+            appId: _debug.enabled("dev:shougun:cast")
+                ? DEV_ID
+                : PROD_ID,
         });
     }
 }
