c1dc7696623a89dc1fd5c6a7175798f195cccb97
Daniel Leong <me@dhleong.net>
2019-11-02 20:12:20 -0400

    Add a means of starting a title from RPC search() result

--- src/rpc/handler.ts
+++ src/rpc/handler.ts
@@ -1,3 +1,5 @@
+import { DefaultMatcher } from "../match/default";
+import { IMedia } from "../model";
 import { Shougun } from "../shougun";
 
 export class RpcHandler {
@@ -7,12 +9,30 @@ export class RpcHandler {
 
     public async search(query: string) {
         const media = await this.shougun.search(query);
-        const sorted = this.shougun.context.matcher.sort(
+
+        const matcher = new DefaultMatcher();
+        const sorted = matcher.sort(
             query,
             media,
             item => item.title,
         );
+
+        // return a subset; results after this point are
+        // usually garbage anyway
         return sorted.slice(0, 20);
+    }
+
+    public async start(media: IMedia) {
+        const candidates = await this.shougun.search(media.title);
+        if (!candidates) throw new Error(`No results for ${media.title}`);
+
+        for (const c of candidates) {
+            if (c.discovery === media.discovery && c.id === media.id) {
+                return this.shougun.play(c);
+            }
+        }
+
+        throw new Error(`No media with title ${media.title} and ID ${media.id}`);
     }
 
     public async startByTitle(title: string) {
