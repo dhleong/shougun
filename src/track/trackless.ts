ec9ec8da835b4b47252795c08df8357c0a33d65b
Daniel Leong <me@dhleong.net>
2019-12-04 09:46:21 -0500

    Stub takeout methods in TracklessTracker

--- src/track/trackless.ts
+++ src/track/trackless.ts
@@ -1,11 +1,17 @@
 import { IMedia, ISeries, isSeries } from "../model";
 import { IBorrowedData, ITakeoutTrack, ITrack, ITracker } from "./base";
+import { IViewedInformation } from "./persistent";
 
 export class TracklessTracker implements ITracker {
+    public markBorrowReturned(tokens: string[]): Promise<void> {
+        throw new Error("Takeout not supported");
+    }
+    public returnBorrowed(tokens: string[], viewedInformation: IViewedInformation[]): Promise<void> {
+        throw new Error("Takeout not supported");
+    }
     public createTakeout(track: ITakeoutTrack): Promise<void> {
         throw new Error("Takeout not supported");
     }
-
     public retrieveBorrowed(): Promise<IBorrowedData> {
         throw new Error("Takeout not supported");
     }
