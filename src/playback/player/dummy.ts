46c3cb5829ce48a8356d835d2d79b31c52d9df93
Daniel Leong <me@dhleong.net>
2019-12-04 11:08:24 -0500

    Fix lint warnings in DummyPlayer

--- src/playback/player/dummy.ts
+++ src/playback/player/dummy.ts
@@ -1,23 +1,20 @@
-import { IPlaybackOptions, IPlayer, IPlayerCapabilities } from "../player";
 import { Context } from "../../context";
-import { IPlayable } from "../../model";
 import { IAudioTrack, IVideoTrack } from "../../media/analyze";
+import { IPlayable } from "../../model";
+import { IPlaybackOptions, IPlayer, IPlayerCapabilities } from "../player";
 
 export class DummyPlayer implements IPlayer {
     public async getCapabilities(): Promise<IPlayerCapabilities> {
         return {
             supportsAudioTrack: (track: IAudioTrack) => true,
             supportsVideoTrack: (track: IVideoTrack) => true,
+
             supportsContainer: (container: string) => true,
             supportsPixelFormat: (format: string) => true,
         };
     }
 
     public async play(context: Context, playable: IPlayable, options?: IPlaybackOptions): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-
-    showRecommendations?(context: import("../../context").Context, recommendations: Promise<import("../../model").IMedia[]>): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
