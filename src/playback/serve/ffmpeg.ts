d350ddeec2cb7713cd343078dcc61b4155f22e4a
Daniel Leong <me@dhleong.net>
2021-07-09 18:51:22 -0400

    Refactor to share ffmpeg stream init logic

---
+++ src/playback/serve/ffmpeg.ts
@@ -0,0 +1,58 @@
+import _debug from "debug";
+const debug = _debug("shougun:serve:ffmpeg");
+
+import ffmpeg from "fluent-ffmpeg";
+import stream from "stream";
+
+export interface IFfmpegOpts {
+    autoEnd?: boolean;
+    config?: (cmd: ffmpeg.FfmpegCommand) => void,
+}
+
+export const ffmpegAsPromise = (
+    logContext: string,
+    command: ffmpeg.FfmpegCommand,
+    opts: IFfmpegOpts = {},
+    pipe: stream.PassThrough = new stream.PassThrough(),
+) => new Promise<stream.PassThrough>((resolve, reject) => {
+    command
+        .on("start", cmd => {
+            debug("start", logContext, ":", cmd);
+        })
+        .once("progress", data => {
+            debug("progress @", logContext);
+            resolve(pipe);
+        })
+        .on("error", e => {
+            debug("error", logContext, e);
+            reject(e);
+        })
+        .on("end", () => {
+            debug("done", logContext);
+            pipe.end();
+        });
+
+    if (opts.config) {
+        opts.config(command);
+    }
+
+    pipe.once("close", () => {
+        // the user stopped viewing the stream; stop transcoding
+        debug("pipe closed; shut down:", logContext);
+        command.kill("SIGKILL");
+    });
+
+    // don't end it automatically (IE on error); we'll do it
+    // ourselves (see above)
+    const end = opts.autoEnd === true;
+    command.output(pipe, { end }).run();
+
+    // HACKS: if we don't get an error *or* otherwise resolve in 1s,
+    // just resolve so we can *try* to read from the pipe.
+    // TODO: Perhaps if `pipe` read from the command output but stored
+    // it in a buffer until downstream was ready to consume, it'd work
+    // without this hack?
+    setTimeout(() => {
+        resolve(pipe);
+    }, 1000);
+});
--- src/playback/serve/subtitle.ts
+++ src/playback/serve/subtitle.ts
@@ -1,48 +1,21 @@
 import _debug from "debug";
-const debug = _debug("shougun:subtitle");
 
 import ffmpeg from "fluent-ffmpeg";
-import stream = require("stream");
 
 import { ITextTrack } from "../../media/analyze";
+import { ffmpegAsPromise, IFfmpegOpts } from "./ffmpeg";
 
-export const extractSubtitlesTrack = (
+export function extractSubtitlesTrack(
     localPath: string,
     track: ITextTrack,
-    opts: { autoEnd?: boolean } = {},
-) => new Promise<stream.PassThrough>((resolve, reject) => {
-    const pipe = new stream.PassThrough();
-
+    opts: IFfmpegOpts = {},
+) {
     // NOTE: Using the .map() method causes the library to wrap our stream
     // selection in [brackets] which apparently breaks it? Not sure what's
     // the deal there....
     const command = ffmpeg(localPath)
         .outputOption("-map", `0:${track.index}`)
-        .outputFormat("webvtt")
-        .on("error", e => {
-            debug("error extracting subtitles", localPath, e);
-            reject(e);
-        })
-        .on("end", () => {
-            debug("done extracting subtitles @", localPath);
-            pipe.end();
-        });
+        .outputFormat("webvtt");
 
-    pipe.once("close", () => {
-        // the user stopped viewing the stream; stop transcoding
-        debug("pipe closed; shut down transcode");
-        command.kill("SIGKILL");
-    });
-
-    // don't end it automatically (IE on error); we'll do it
-    // ourselves (see above)
-    const end = opts.autoEnd === true;
-    command.output(pipe, { end }).run();
-
-    debug("end=", end);
-
-    // See HACKS in transcode.ts
-    setTimeout(() => {
-        resolve(pipe);
-    }, 1000);
-});
+    return ffmpegAsPromise(`extract subtitles from ${localPath}`, command, opts);
+}
